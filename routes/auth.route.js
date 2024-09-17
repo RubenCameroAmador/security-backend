17e32fbf75a5d459be747b6c71b924025bce1a2c
Laura Marcela Arango Correa <laura.arango@mercadolibre.com.co>
2024-09-17 18:19:03 -0500

    Added rate limiting

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import { login, register } from "../controllers/auth.controller.js";
+import rateLimit from 'express-rate-limit';
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // limit each IP to 10 requests per windowMs
+    message: {
+        status: 429,
+        message: "Too many requests, please try again later."
+    }
+})
 
 const auth_router = Router()
 
-auth_router.post('/register', register)
+auth_router.post('/register', authLimiter, register)
 
-auth_router.post('/login', login)
+auth_router.post('/login', authLimiter, login)
 
 
-export { auth_router } 
+export { auth_router }
